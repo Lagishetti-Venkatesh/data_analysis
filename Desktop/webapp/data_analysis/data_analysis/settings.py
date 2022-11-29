384206157aaa928555ff89c39a648433af072912
Lagishetti venkatesh <58334104+Lagishetti-Venkatesh@users.noreply.github.com>
2022-11-29 16:01:55 +0530

    hidden the secret key

--- Desktop/webapp/data_analysis/data_analysis/settings.py
+++ Desktop/webapp/data_analysis/data_analysis/settings.py
@@ -25,7 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-3wu7tcidl4p!1kc-&hx=ekqpnrsma8xidzai8rrh66_ya(n#u0'
+SECRET_KEY = env(['SECRET_KEY'])
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
